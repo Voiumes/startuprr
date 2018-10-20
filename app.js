e1e49ef52428456d8f48963fd3e988c17295e7c2
Sean <omerta201@yahoo.com>
2018-10-20 19:24:14 -0400

    -

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get("/", function (req, res) {
     res.render('index');
 });
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
 
-// app.listen(3000, function () {
-//     console.log("Server is live on Port 3000");
-// });
+app.listen(3000, function () {
+    console.log("Server is live on Port 3000");
+});
